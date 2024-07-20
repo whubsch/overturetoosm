6322c27e37b6cd4838540f030e7d9e1da0b975e0
Will <wahubsch@gmail.com>
2024-07-19 20:39:25 -0400

    Adding typing

--- process.py
+++ process.py
@@ -1,47 +1,111 @@
+from typing import Any, Optional
 from resources import tags
+import pydantic
 
 bad_tags = ["id", "version", "update_time", "sources", "user", "uid"]
 
 
-def process_geojson_feature(
-    feature: dict[str, str | dict | list],
+class Sources(pydantic.BaseModel):
+    property: str
+    dataset: str
+    record_id: str
+    confidence: float | None
+
+
+class Names(pydantic.BaseModel):
+    primary: str
+    common: str | None
+    rules: str | None
+
+
+class Addresses(pydantic.BaseModel):
+    freeform: str | None
+    locality: str | None
+    postcode: str | None
+    region: str | None
+    country: str | None
+
+
+class Categories(pydantic.BaseModel):
+    main: str
+    alternate: list[str] | None
+
+
+class Brand(pydantic.BaseModel):
+    wikidata: str | None
+    names: Names
+
+
+class PlaceProps(pydantic.BaseModel):
+    id: str
+    version: int
+    update_time: str
+    sources: list[Sources]
+    names: Names
+    brand: Brand | None = None
+    categories: Categories | None = None
+    confidence: float
+    websites: list[str] | None = None
+    socials: list[str] | None = None
+    phones: list[str] | None = None
+    addresses: list[Addresses]
+
+
+def process_geojson_props(
+    props: dict,
     region_tag: str = "addr:state",
     confidence: float = 0.0,
-) -> None:
+) -> dict[str, str]:
+    """Process GeoJSON properties from Overture."""
     new_props = {}
-    props: dict = feature["properties"]
+    prop_obj = PlaceProps(**props)
+    if prop_obj.confidence < confidence:
+        return {}
 
     try:
-        prim = tags.get(props["categories"]["main"])
-        if prim:
-            new_props |= prim
-        else:
-            print(props["categories"]["main"])
+        if prop_obj.categories:
+            prim = tags.get(prop_obj.categories.main)
+            if prim:
+                new_props |= prim
+            else:
+                print(prop_obj.categories.main)
     except KeyError:
         pass
 
     try:
-        name = props["names"]["primary"]
+        name = prop_obj.names.primary
         if name:
             new_props |= {"name": name}
     except KeyError:
         pass
 
     if "phones" in props:
-        new_props["phone"] = props["phones"][0]
+        new_props["phone"] = prop_obj.phones[0]
 
     if "websites" in props:
-        new_props["website"] = props["websites"][0]
+        if prop_obj.websites:
+            new_props["website"] = prop_obj.websites[0]
 
     if "addresses" in props:
         add = props["addresses"][0]
         if "freeform" in add:
-            new_props["addr:street_address"] = add["freeform"]
+            new_props["addr:street_address"] = prop_obj.addresses[0].freeform
         if "country" in add:
-            new_props["addr:country"] = add["country"]
+            new_props["addr:country"] = prop_obj.addresses[0].country
         if "postcode" in add:
-            new_props["addr:postcode"] = add["postcode"]
+            new_props["addr:postcode"] = prop_obj.addresses[0].postcode
         if "region" in add:
-            new_props[region_tag] = add["region"]
+            new_props[region_tag] = prop_obj.addresses[0].region
 
-    # print(new_props)
+    if "sources" in props:
+        new_props["source"] = (
+            ", ".join({i["dataset"] for i in props["sources"]}) + " via overture_to_osm"
+        )
+
+    if "brand" in props:
+        if prop_obj.brand:
+            new_props["brand"] = prop_obj.brand.names.primary
+            new_props["brand:wikidata"] = prop_obj.brand.wikidata
+
+    print(new_props)
+    return new_props
--- test.py
+++ test.py
@@ -1,22 +1,27 @@
 from collections import Counter
 import json
-from process import process_geojson_feature
+from process import process_geojson_props
 from resources import tags
 
-with open("arlington_over.geojson", "r", encoding="utf-8") as f:
+with open("arlington_over1.geojson", "r", encoding="utf-8") as f:
     contents: dict = json.load(f)
     count = []
 
     for each in contents["features"]:
-        # process_geojson_feature(each)
-        try:
-            prim = tags.get(each["properties"]["categories"]["main"])
-            if not prim:
-                # print(each["properties"]["categories"]["main"])
-                count.append(each["properties"]["categories"]["main"])
+        each["properties"] = process_geojson_props(each["properties"], confidence=0.8)
+        # try:
+        #     prim = tags.get(each["properties"]["categories"]["main"])
+        #     if not prim:
+        #         # print(each["properties"]["categories"]["main"])
+        #         count.append(each["properties"]["categories"]["main"])
 
-        except KeyError:
-            pass
+        # except KeyError:
+        #     pass
 
-    print(len(count), len(contents["features"]))
-    print(Counter(count))
+    contents["features"] = [each for each in contents["features"] if each["properties"]]
+
+    with open("arlington_over2.geojson", "w+", encoding="utf-8") as f:
+        json.dump(contents, f, indent=4)
+
+    # print(len(count), len(contents["features"]))
+    # print(Counter(count))
