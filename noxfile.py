d07f2a804f71483425fb2241f1ac8aee608e3606
Will <wahubsch@gmail.com>
2025-12-09 22:05:39 -0500

    resolving python version conflict, adding 3.14 to testing

--- noxfile.py
+++ noxfile.py
@@ -19,7 +19,7 @@ import tempfile
 import os
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session):
     """Run tests with pytest across multiple Python versions."""
     session.install("pytest", "pydantic")
